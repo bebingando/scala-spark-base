3648ac1d778e630f65e9e7bc2040f5cbf5fb534a
Steve Black <sblack@paytronix.com>
2020-08-04 13:00:00 -0400

    Initial commit

---
+++ src/main/scala/HelloWorld.scala
@@ -0,0 +1,14 @@
+import org.apache.spark.{SparkConf, SparkContext}
+
+object HelloWorld {
+    def main(args: Array[String]): Unit = {
+        val conf = new SparkConf()
+            .setMaster("local")
+            .setAppName("LearnScalaSpark")
+        val sc = new SparkContext(conf)
+        sc.setLogLevel("ERROR")
+
+        val helloWorldString = "Hello World!"
+        println(helloWorldString)
+    }
+}
